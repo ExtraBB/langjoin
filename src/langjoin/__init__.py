8a81de0b68e03723c83b177875cdd285ac4c5c80
Bruno Carvalhal <b.carvalhal@outlook.com>
2024-10-25 11:18:47 +0200

    Implement replace function

--- src/langjoin/__init__.py
+++ src/langjoin/__init__.py
@@ -1,6 +1,13 @@
 from langjoin import epub
+from langjoin.replace import replace_noun_chunks
 
 def main() -> int:
-    epub.load("static/epub/monkey.epub")
-    print("ok")
+    replaced = replace_noun_chunks("The bear thinks about becoming a politician to make a greater impact on the world.", replace_func)
+    print(replaced)
     return 0
+
+def replace_func(input: str) -> str:
+    if(input[0].isupper()):
+        return "The man"
+    else:
+        return "the man"
--- src/langjoin/epub.py
+++ src/langjoin/epub.py
@@ -1,16 +1,14 @@
 import epubfile
-import spacy
 
 def load(path: str):
     book = epubfile.Epub(path)
-    nlp = spacy.load("en_core_web_sm")
                 
     for text_id in book.get_texts():
         soup = book.read_file(text_id, soup=True)
-        for p in soup.find_all("p"):
+        # for p in soup.find_all("p"):
 
-            if (len(p.text) > 100):
-                doc = nlp(p.text)
-                sentences = [sent.text for sent in doc.sents]
-                print(sentences)
-                return
+        #     if (len(p.text) > 100):
+        #         doc = nlp(p.text)
+        #         sentences = [sent.text for sent in doc.sents]
+        #         print(sentences)
+        #         returns
