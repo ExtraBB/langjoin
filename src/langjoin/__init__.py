c1d6ccc3fdf8b36e3506613c3ca421e607fcf616
Bruno Carvalhal <b.carvalhal@outlook.com>
2024-10-26 18:06:15 +0200

    Add (broken) epub transform logic

--- src/langjoin/__init__.py
+++ src/langjoin/__init__.py
@@ -1,10 +1,17 @@
+from langjoin.epub import transform_epub
 from langjoin.transform_local import transform_text as transform_text_local
 from langjoin.transform_llm import transform_text as transform_text_llm
 
 def main() -> int:
+    transform_epub("static/epub/monkey.epub", 5, epub_transformer)
+    return 0
+
+def test_transforms():
     text = "She got a mug from the cabinet. She set it beneath the machine’s dispenser. She opened the machine’s lid. Something began to buzz. She reached into the cabinet again. She retrieved an off-brand K-cup. The buzzing grew louder. She put the cup in the coffee maker. She closed the lid. The crunch felt violent. She selected ‘8oz’ on the little touch screen. The buzzing became a drone. She pressed the button beneath the touch screen. The machine began to whir. She could barely hear it over the droning. The floor creaked behind her. Something was watching her."
     transformed_local = transform_text_local(text, "pt", ["verb"], 0.7)
     transformed_llm = transform_text_llm(text, "pt", ["verb"], 0.7)
     print("LOCAL: " + transformed_local + "\n\n")
     print("LLM: " + transformed_llm + "\n\n")
-    return 0
+    
+def epub_transformer(text: str) -> str:
+    return transform_text_llm(text, "pt", ["verb", "noun", "adjective"], 0.7)
--- src/langjoin/epub.py
+++ src/langjoin/epub.py
@@ -1,14 +1,18 @@
+from typing import Callable
 import epubfile
 
-def load(path: str):
+def transform_epub(path: str, max_paragraphs: int, transformer: Callable[[str], str]):
     book = epubfile.Epub(path)
-                
+    paragraphs_transformed = 0
+    
     for text_id in book.get_texts():
         soup = book.read_file(text_id, soup=True)
-        # for p in soup.find_all("p"):
-
-        #     if (len(p.text) > 100):
-        #         doc = nlp(p.text)
-        #         sentences = [sent.text for sent in doc.sents]
-        #         print(sentences)
-        #         returns
+        for p in soup.find_all("p"):
+            if len(p.text) > 100:
+                new_text = transformer(p.text)
+                print("Replacing:\n\n" + p.text + "\n\nWith:\n\n" + new_text)
+                p.string.replace_with(new_text)
+                paragraphs_transformed += 1
+                if paragraphs_transformed >= max_paragraphs:
+                    return
+        book.write_file(text_id, soup)
